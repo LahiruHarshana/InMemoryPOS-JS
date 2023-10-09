e75bd8a0fb67eaaf27d29294b5e0b51ede009bfb
lahiruharshana <lharshana2002@gmail.com>
2023-10-10 00:58:10 +0530

    content updated

--- InMemoryPOS/controllers/CustomerController.js
+++ InMemoryPOS/controllers/CustomerController.js
@@ -119,9 +119,6 @@ $("#cDeleteBtn").click(() => {
 
 $("#cUpdateBtn").click(() => {
     const cIdValue = $cIdTxt.val();
-    const cNameValue = $cNameTxt.val();
-    const cAddressValue = $cAddressTxt.val();
-    const cSalaryValue = $cSalaryText.val();
 
     for (let i = 0; i < Customers.length; i++) {
         if (Customers[i].id === cIdValue) {
