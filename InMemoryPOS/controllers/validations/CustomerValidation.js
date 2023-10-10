225a280b09b8f0f2d81ce18fcd792dba0cfef535
lahiruharshana <lharshana2002@gmail.com>
2023-10-10 23:05:06 +0530

    updated

--- InMemoryPOS/controllers/validations/CustomerValidation.js
+++ InMemoryPOS/controllers/validations/CustomerValidation.js
@@ -2,3 +2,8 @@ const CUS_ID_REGEX = /^(C00-)[0-9]{3}$/;
 const CUS_NAME_REGEX = /^[A-Za-z ]{5,}$/;
 const CUS_ADDRESS_REGEX = /^[A-Za-z0-9 ]{8,}$/;
 const CUS_SALARY_REGEX = /^[0-9]{2,}([.][0-9]{2})?$/;
+
+
+$("#cIdTxt").input(function () {
+    CUS_ID_REGEX.test()
+});
