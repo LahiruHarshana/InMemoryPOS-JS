74e9ab181fa906b7c714aa12b77f2baf59dc94a1
lahiruharshana <lharshana2002@gmail.com>
2023-10-10 21:17:02 +0530

    updated

--- InMemoryPOS/controllers/PurchaseOrderController.js
+++ InMemoryPOS/controllers/PurchaseOrderController.js
@@ -1,38 +1,35 @@
 import Customers from './CustomerController.js';
 import Items from "./ItemController.js";
 
-
-$("#orderNav").click(function (){
-
+$("#orderNav").click(function () {
+    const customerFormVar = document.querySelector("#customerForm");
+    const itemFormVar = document.querySelector("#itemForm");
+    const orderrFormVar = document.querySelector("#orderForm");
 
     const selectElement = $("#selectCustomerId");
-
     selectElement.children().remove();
 
-    for (let i = 0; i <Customers.length; i++) {
+    for (let i = 0; i < Customers.length; i++) {
         const option = $("<option></option>");
         option.val(Customers[i].id);
         option.text(Customers[i].id);
         selectElement.append(option);
     }
 
-
     const selectItem = $("#oSelectItem");
-
     selectItem.children().remove();
 
-    for (let i = 0; i <Items.length; i++) {
+    for (let i = 0; i < Items.length; i++) {
         const option = $("<option></option>");
         option.val(Items[i].id);
         option.text(Items[i].id);
         selectItem.append(option);
     }
-    customerFormVar.style.display="none";
-    itemFormVar.style.display="none";
-    orderrFormVar.style.display="inline";
+
+    customerFormVar.style.display = "none";
+    itemFormVar.style.display = "none";
+    orderrFormVar.style.display = "inline";
 });
-
-
 $("#selectCustomerId").change(function () {
     const selectedValue = $(this).val();
 
@@ -42,12 +39,12 @@ $("#selectCustomerId").change(function () {
     }
 
     const customer = Customers.find((c) => c.id === selectedValue);
+    console.log(customer);
 
     if (customer) {
-        $("").val(customer.name);
-        $cIdTxt.val(customer.id);
-        $cAddressTxt.val(customer.address);
-        $cSalaryText.val(customer.salary);
+        $("#oCName").val(customer.name);
+        $("#oCAddress").val(customer.address);
+        $("#oCSalary").val(customer.salary);
     } else {
         alert("Customer not found.");
     }
