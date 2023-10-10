5591e8c9a9653dbf2217d0f43ca0fde68d192f1f
lahiruharshana <lharshana2002@gmail.com>
2023-10-10 23:22:15 +0530

    updated

--- InMemoryPOS/controllers/validations/CustomerValidation.js
+++ InMemoryPOS/controllers/validations/CustomerValidation.js
@@ -4,6 +4,50 @@ const CUS_ADDRESS_REGEX = /^[A-Za-z0-9 ]{8,}$/;
 const CUS_SALARY_REGEX = /^[0-9]{2,}([.][0-9]{2})?$/;
 
 
-$("#cIdTxt").input(function () {
-    CUS_ID_REGEX.test()
-});
+$("#cIdTxt").keyup(function (e) {
+    let value = $("#cIdTxt").val();
+    if (value.length == 0) {
+    } else {
+        let res = CUS_ID_REGEX.test(value);
+        if (res) {
+            $("#cIdTxt").css('border', '2px solid green');
+        } else {
+            $("#cIdTxt").css('border', '2px solid red');
+        }
+    }});
+
+$("#cNameTxt").keyup(function (e) {
+    let value = $("#cNameTxt").val();
+    if (value.length == 0) {
+    } else {
+        let res = CUS_NAME_REGEX.test(value);
+        if (res) {
+            $("#cNameTxt").css('border', '2px solid green');
+        } else {
+            $("#cNameTxt").css('border', '2px solid red');
+        }
+    }});
+
+$("#cAddressTxt").keyup(function (e) {
+    let value = $("#cAddressTxt").val();
+    if (value.length == 0) {
+    } else {
+        let res = CUS_ADDRESS_REGEX.test(value);
+        if (res) {
+            $("#cAddressTxt").css('border', '2px solid green');
+        } else {
+            $("#cAddressTxt").css('border', '2px solid red');
+        }
+    }});
+
+$("#cSalaryText").keyup(function (e) {
+    let value = $("#cSalaryText").val();
+    if (value.length == 0) {
+    } else {
+        let res = CUS_SALARY_REGEX.test(value);
+        if (res) {
+            $("#cSalaryText").css('border', '2px solid green');
+        } else {
+            $("#cSalaryText").css('border', '2px solid red');
+        }
+    }});
